53f03598526ce0593a3ceb0ee28916dd1f136235
Kavi <kavi4july@gmail.com>
2016-07-27 17:24:44 +0530

    Create num_as_string.java

---
+++ num_as_string.java
@@ -0,0 +1,11 @@
+public class num_as_string
+{
+  public static void main(String args[])
+  {
+    String n1="5";
+    String n2="10";
+     int i  = Integer.parseInt(n1); 
+      int j = Integer.parseInt(n2); 
+      System.out.println(i*j);
+    }
+  }
