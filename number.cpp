cefc9caa1cb60448c24e23ece6211aae31bee9aa
Kavi <kavi4july@gmail.com>
2016-08-03 17:47:17 +0530

    Create number.cpp

---
+++ number.cpp
@@ -0,0 +1,12 @@
+include<iostream.h>
+#include<conio.h>
+void main()
+{
+    int num;
+    cout<<"Enter a number : ";
+    cin>>num;
+    if(n==0) cout<<"\nZero";
+    else if(n>0) cout<<"\nPositive Number ";
+    else cout<<"\n Negative Number ";
+    getch();
+    }
