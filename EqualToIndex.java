a2572468dfb5bb4fb9eea364d7a037c9de69fbb9
Kavi <kavi4july@gmail.com>
2016-12-12 09:53:46 +0530

    Create EqualToIndex.java

---
+++ EqualToIndex.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+class EqualToIndex
+{
+	public static void main(String args[])
+	{
+  		Scanner in = new Scanner());
+  		int arr = new int [20];
+  		int N=args.lenth
+  		for(int i=0;i<=N;i++)
+  			arr[i]=in.nextInt;
+  		for(i=0;i<=N-1;i++){
+  				if(arr[i]==i)
+  					System.out.println(arr[i]);
+  			}
+  	}
+  }	
