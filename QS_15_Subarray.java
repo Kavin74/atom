3c85f1fd4356f1ef78df7e99ee920d444ebb56da
Kavi <kavi4july@gmail.com>
2016-12-19 13:42:33 +0530

    Update QS_15_Subarray.java

--- QS_15_Subarray.java
+++ QS_15_Subarray.java
@@ -1,19 +1,19 @@
 import java.util.*;
-public class sub_array
+public class QS_15_Subarray
 {
-public static void main(String ar[])
+public static void main(String args[])
 {
 Scanner in=new Scanner(System.in);
-int n,x;
-n=in.nextInt();
+int num,x;
+num=in.nextInt();
 int a[]=new int[n];
-for(int i=0;i<n;i++)
+for(int i=0;i<num;i++)
 {
 a[i]=in.nextInt();
 }
-for(int i=0;i<n-1;i++)
+for(int i=0;i<num-1;i++)
 {
-for(int j=i+1;j<n;j++)
+for(int j=i+1;j<num;j++)
 {
 if(a[j]>a[i])
 {
