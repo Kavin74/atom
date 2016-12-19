a8e55262164e02ed4fa0d42f07832d440f044d74
Kavi <kavi4july@gmail.com>
2016-12-19 13:28:00 +0530

    Add files via upload

---
+++ QS12_ArrayOfHeight.java
@@ -0,0 +1,14 @@
+public class QS12_ArrayOfHeight
+{
+	public static void main(String args)
+	{
+		Scanner in = new Scanner(System.in);
+		arr[] = new int[55];
+		sarray[] = new int[55];
+		for(int i=0; i<50;i++)
+			arr[i]=in.readInt;
+		sarray[] =java.util.Array.sort(arr[]);
+		System.out.println("Fourth Tallest Student : "+sarray[46]);
+		System.out.println("Kth Tallest Student : "+sarray[49]);
+	}
+}
